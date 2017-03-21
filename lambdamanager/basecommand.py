163a6b087c9b922ed3feb9de557fe4e9790947d3
ant30 <antonio.perezaranda@audiense.com>
2017-03-21 16:45:15 +0100

    Help improved, now command help runs

---
+++ lambdamanager/basecommand.py
@@ -0,0 +1,21 @@
+
+class BaseCommand(object):
+
+    command_doc = __doc__
+    EXIT_OK = 0
+
+    def __init__(self, manager):
+        self.aws_lambda = manager
+
+    @classmethod
+    def usage(self):
+        """Formated usage"""
+        return "{0}\n{1}\n".format(self.__doc__, self.command_usage())
+
+    @classmethod
+    def command_usage(self):
+        """Get the command usage"""
+        raise NotImplementedException()
+
+    def __call__(self):
+        raise NotImplementedException()
