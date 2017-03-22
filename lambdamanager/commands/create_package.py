6ecfbe81d92ea210773102d2953aa310d034757c
ant30 <antonio.perezaranda@audiense.com>
2017-03-22 17:44:27 +0100

    Implemented requirements.txt support in pytho27 packages

--- lambdamanager/commands/create_package.py
+++ lambdamanager/commands/create_package.py
@@ -10,3 +10,4 @@ class CreateFunctionPackage(BaseCommand):
 
     def __call__(self, arguments):
         self.aws_lambda.create_package(arguments['<alias_name>'] or 'devel')
+        print("The package name is: {0}".format(self.aws_lambda.local_filename))
