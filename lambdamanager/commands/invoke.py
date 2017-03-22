dad44c6f4c6ab3b07731362b7f642d59c95eae5e
ant30 <antonio.perezaranda@audiense.com>
2017-03-22 15:07:53 +0100

    Implemented payload from command or stding in invoke.py

--- lambdamanager/commands/invoke.py
+++ lambdamanager/commands/invoke.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, print_function
 
 from pprint import pprint
+from os import path
 import sys
 
 from ..basecommand import BaseCommand
@@ -10,26 +11,35 @@ class InvokeFunction(BaseCommand):
     """ invoke: Execute the remote function code """
 
     subcommand_name = 'invoke'
-    extra_args = '[--async] [--qualifier=QUALIFIER] [--payload=PAYLOAD_FILE]'
+    extra_args = '[--payload=FILE] [--async] [--qualifier=QUALIFIER]'
     extra_args_body = '\n'.join([
         "--async    To launch the function in the async way.",
+        "--payload FILE   The filename with the payload to send to the function.",
         "--qualifier QUALIFIER    The alias or function version number [default: devel]",
-        "--payload PAYLOAD_FILE     The file with the payload to send to the function.",
+        "",
+        "   Note: If payload is not defined, then taking payload from stdin",
+        "   You can do:",
+        "   {command} {subcommand} --qualifier alias < payload.json",
     ])
 
     def __call__(self, arguments):
 
-        print(arguments)
-        return
-
         if not self.aws_lambda.function_exists():
             print("Lambda function does not exists")
-            sys.exit(1)
-
+            exit(1)
         qualifier = arguments['--qualifier']
+        payload_filename = arguments['--payload']
 
         func = (self.aws_lambda_invoke_async
-                if self.options.async
+                if arguments['--async']
                 else self.aws_lambda.invoke_sync)
 
-        return func(str(qualifier), self.options.payload)
+        if payload_filename and path.isfile(payload_filename):
+            payload = open(payload_filename, 'r')
+        else:
+            payload = sys.stdin
+
+        pprint(func(qualifier, payload))
+
+        if payload_filename:
+            payload.close()
